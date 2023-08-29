c5a50afade79043f8f33efb207d3e920400689a1
Karen Gomes <kngs@ic.ufal.br>
2023-08-29 17:11:21 -0300

    add leaflet map

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,24 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
+import 'leaflet-defaulticon-compatibility'
+
+const Map = () => (
+  <MapContainer
+    center={[0, 0]}
+    zoom={3}
+    style={{ height: '100%', width: '100%' }}
+  >
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[0, 0]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+)
+
+export default Map
