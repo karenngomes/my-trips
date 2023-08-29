4572ce42c3ee95dbe9bb9098847c68bcaee53601
Karen Gomes <kngs@ic.ufal.br>
2023-08-29 17:56:47 -0300

    fix jest error

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
-module.exports = {
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
@@ -7,5 +15,15 @@ module.exports = {
   modulePaths: ['<rootDir>/src/'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
-  }
-}
+  },
+  
+  
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = async () => ({
+  ...(await createJestConfig(customJestConfig)()),
+  transformIgnorePatterns: [
+    './node_modules/(?!(react-leaflet/lib|@react-leaflet/core/lib)/)'
+  ]
+})
